67147c2f2cbf7303fa151f798b7817f65e71ac5a
Dinaaz Codes <dinaazshaikh1808@gmail.com>
2022-12-20 19:21:15 +0530

    add web3 integration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,23 +5,42 @@ import usePrice from "./hooks/usePrice";
 import Header from "./components/header/Header";
 import ContractDetails from "./components/ContractDetails";
 import ContributeForm from "./components/ContributeForm";
+import useWeb3 from "./hooks/useWeb3";
+
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
 
 function App() {
+  const { isConnected, connectWallet, activeAccount, walletBalance } =
+    useWeb3();
   const { getDollarValue } = usePrice();
+
+  const getContributeForm = () => {
+    
+    return (
+      <Card className="mt-4 p-4">
+        <Card.Subtitle>Balance : {walletBalance.toFixed(4)} ETH </Card.Subtitle>
+        <ContributeForm />
+      </Card>
+    );
+  };
+
   return (
     <>
-      <Header isConnected={false} />
+      <Header isConnected={isConnected} walletAddress={activeAccount} />
       <Container>
         <Row className="mt-5">
           <Col lg={8}>
             <Contributions
               totalAmtInDollar={getDollarValue()}
               totalAmtInEth={5.2}
+              connectWalletHandler={connectWallet}
+              isConnected={isConnected}
             />
-            <Card className="mt-4 p-4">
-              <Card.Subtitle>Balance : 0.8 ETH</Card.Subtitle>
-              <ContributeForm />
-            </Card>
+            {isConnected && getContributeForm()}
           </Col>
           <Col>
             <ContractDetails />
--- client/src/components/Contributions.tsx
+++ client/src/components/Contributions.tsx
@@ -3,16 +3,18 @@ import CustomButton from "./CustomButton";
 
 type Props = {
   totalAmtInEth: number;
-  totalAmtInDollar:number
+  totalAmtInDollar:number;
+  connectWalletHandler:()=>Promise<void>;
+  isConnected:boolean
 };
 
-const Contributions = ({ totalAmtInEth ,totalAmtInDollar  }: Props) => {
+const Contributions = ({ totalAmtInEth ,totalAmtInDollar , connectWalletHandler , isConnected }: Props) => {
   
   return (
     <Card className="text-center">
       <Card.Body>
         <Card.Title>Total Contribution : ${totalAmtInDollar} ( {`${totalAmtInEth} Eth`})</Card.Title>
-        <CustomButton onClickHandler={()=>{}} variant="primary" text="Connect with Metamask" />
+         {!isConnected && <CustomButton onClickHandler={connectWalletHandler} variant="primary" text="Connect with Metamask" />}
       </Card.Body>
     </Card>
   );
